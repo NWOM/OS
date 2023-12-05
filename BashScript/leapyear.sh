11ffbece83fff9414cfbd1bc2fc4e938af6cdd15
NWOM <98202572+NWOM@users.noreply.github.com>
2023-12-06 02:23:19 +0530

    Create leapyear.sh

---
+++ BashScript/leapyear.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Enter a year: "
+read year
+
+if [ $((year % 4)) -eq 0 ] && [ $((year % 100)) -ne 0 ] || [ $((year % 400)) -eq 0 ]; then
+    echo "The year $year is a leap year."
+else
+    echo "The year $year is not a leap year."
+fi
