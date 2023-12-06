e63a55cc6d9d215448e7ec9d251910b4b11987e6
NWOM <98202572+NWOM@users.noreply.github.com>
2023-12-06 22:14:11 +0530

    Create swap.sh

---
+++ BashScript/swap.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Initial values
+num1=10
+num2=20
+
+echo "Before swapping:"
+echo "Number 1: $num1"
+echo "Number 2: $num2"
+
+# Swapping logic
+temp=$num1
+num1=$num2
+num2=$temp
+
+echo "After swapping:"
+echo "Number 1: $num1"
+echo "Number 2: $num2"
