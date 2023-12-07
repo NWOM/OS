0367ac33d4f7450615646b9847e2d2bb293786df
NWOM <98202572+NWOM@users.noreply.github.com>
2023-12-07 21:19:51 +0530

    Update
    SJF with non premption

---
+++ SJFnonpre.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+void findWaitingTime(int processes[], int n, int bt[], int wt[]) {
+    wt[0] = 0;
+
+    for (int i = 1; i < n; i++) {
+        wt[i] = bt[i - 1] + wt[i - 1];
+    }
+}
+
+void findTurnAroundTime(int processes[], int n, int bt[], int wt[], int tat[]) {
+    for (int i = 0; i < n; i++) {
+        tat[i] = bt[i] + wt[i];
+    }
+}
+
+void findAvgTime(int processes[], int n, int bt[]) {
+    int wt[n], tat[n], total_wt = 0, total_tat = 0;
+
+    findWaitingTime(processes, n, bt, wt);
+    findTurnAroundTime(processes, n, bt, wt, tat);
+
+    printf("Process   Burst Time   Waiting Time   Turnaround Time\n");
+
+    for (int i = 0; i < n; i++) {
+        total_wt += wt[i];
+        total_tat += tat[i];
+        printf("%d\t\t%d\t\t%d\t\t%d\n", i + 1, bt[i], wt[i], tat[i]);
+    }
+
+    printf("Average waiting time = %.2f\n", (float)total_wt / (float)n);
+    printf("Average turnaround time = %.2f\n", (float)total_tat / (float)n);
+}
+
+int main() {
+    int processes[] = {1, 2, 3};
+    int n = sizeof(processes) / sizeof(processes[0]);
+
+    int burst_time[] = {6, 8, 7};
+
+    findAvgTime(processes, n, burst_time);
+
+    return 0;
+}
+/*OUTPUT
+/tmp/DCuu1kvGrg.o
+Process   Burst Time   Waiting Time   Turnaround Time
+1		6		0		6
+2		8		6		14
+3		7		14		21
+Average waiting time = 6.67
+Average turnaround time = 13.67
