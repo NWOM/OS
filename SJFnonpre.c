2d39609fcb5a874f671b818f399afef8dfe95f49
NWOM <98202572+NWOM@users.noreply.github.com>
2023-12-24 01:33:45 +0530

    Update SJFnonpre.c

--- SJFnonpre.c
+++ SJFnonpre.c
@@ -1,68 +1,95 @@
-#include <stdio.h>
-
-struct Process {
-    int processID;
-    int burstTime;
-};
-
-void swap(struct Process *xp, struct Process *yp) {
-    struct Process temp = *xp;
-    *xp = *yp;
-    *yp = temp;
+<br />#include<stdio.h>
+int main()
+{
+int i,n,p[10]={1,2,3,4,5,6,7,8,9,10},min,k=1,btime=0;
+int bt[10],temp,j,at[10],wt[10],tt[10],ta=0,sum=0;
+float wavg=0,tavg=0,tsum=0,wsum=0;
+printf(" -------Shortest Job First Scheduling ( NP )-------\n");
+printf("\nEnter the No. of processes :");
+scanf("%d",&n);
+ 
+for(i=0;i<n;i++)
+{
+printf("\tEnter the burst time of %d process :",i+1);
+scanf(" %d",&bt[i]);
+printf("\tEnter the arrival time of %d process :",i+1);
+scanf(" %d",&at[i]);
 }
-
-void sortProcesses(struct Process processes[], int n) {
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (processes[j].burstTime > processes[j + 1].burstTime) {
-                swap(&processes[j], &processes[j + 1]);
-            }
-        }
-    }
+ 
+/*Sorting According to Arrival Time*/
+ 
+for(i=0;i<n;i++)
+{
+for(j=0;j<n;j++)
+{
+if(at[i]<at[j])
+{
+temp=p[j];
+p[j]=p[i];
+p[i]=temp;
+temp=at[j];
+at[j]=at[i];
+at[i]=temp;
+temp=bt[j];
+bt[j]=bt[i];
+bt[i]=temp;
 }
-
-void findWaitingTime(struct Process processes[], int n, int waitingTime[]) {
-    waitingTime[0] = 0;
-    for (int i = 1; i < n; i++) {
-        waitingTime[i] = waitingTime[i - 1] + processes[i - 1].burstTime;
-    }
 }
-
-void findTurnAroundTime(struct Process processes[], int n, int waitingTime[], int turnAroundTime[]) {
-    for (int i = 0; i < n; i++) {
-        turnAroundTime[i] = processes[i].burstTime + waitingTime[i];
-    }
 }
-
-void findAverageTime(struct Process processes[], int n) {
-    int waitingTime[n], turnAroundTime[n];
-    float totalWaitingTime = 0, totalTurnAroundTime = 0;
-
-    sortProcesses(processes, n);
-
-    findWaitingTime(processes, n, waitingTime);
-
-    findTurnAroundTime(processes, n, waitingTime, turnAroundTime);
-
-    printf("Process ID\tBurst Time\tWaiting Time\tTurnaround Time\n");
-
-    for (int i = 0; i < n; i++) {
-        totalWaitingTime += waitingTime[i];
-        totalTurnAroundTime += turnAroundTime[i];
-
-        printf("%d\t\t%d\t\t%d\t\t%d\n", processes[i].processID, processes[i].burstTime,
-               waitingTime[i], turnAroundTime[i]);
-    }
-
-    printf("Average waiting time: %.2f\n", totalWaitingTime / n);
-    printf("Average turnaround time: %.2f\n", totalTurnAroundTime / n);
+ 
+/*Arranging the table according to Burst time,
+Execution time and Arrival Time
+Arrival time <= Execution time
+*/
+ 
+for(j=0;j<n;j++)
+{
+btime=btime+bt[j];
+min=bt[k];
+for(i=k;i<n;i++)
+{
+if (btime>=at[i] && bt[i]<min)
+{
+temp=p[k];
+p[k]=p[i];
+p[i]=temp;
+temp=at[k];
+at[k]=at[i];
+at[i]=temp;
+temp=bt[k];
+bt[k]=bt[i];
+bt[i]=temp;
 }
-
-int main() {
-    struct Process processes[] = { {1, 6}, {2, 8}, {3, 7}, {4, 3} };
-    int n = sizeof(processes) / sizeof(processes[0]);
-
-    findAverageTime(processes, n);
-
-    return 0;
+}
+k++;
+}
+wt[0]=0;
+for(i=1;i<n;i++)
+{
+sum=sum+bt[i-1];
+wt[i]=sum-at[i];
+wsum=wsum+wt[i];
+}
+ 
+wavg=(wsum/n);
+for(i=0;i<n;i++)
+{
+ta=ta+bt[i];
+tt[i]=ta-at[i];
+tsum=tsum+tt[i];
+}
+ 
+tavg=(tsum/n);
+ 
+printf("************************");
+printf("\n RESULT:-");
+printf("\nProcess\t Burst\t Arrival\t Waiting\t Turn-around" );
+for(i=0;i<n;i++)
+{
+printf("\n p%d\t %d\t %d\t\t %d\t\t\t%d",p[i],bt[i],at[i],wt[i],tt[i]);
+}
+ 
+printf("\n\nAVERAGE WAITING TIME : %f",wavg);
+printf("\nAVERAGE TURN AROUND TIME : %f",tavg);
+return 0;
 }
