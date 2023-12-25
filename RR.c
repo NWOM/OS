42b23dc8568ecc849890dac9f078571902a53810
NWOM <98202572+NWOM@users.noreply.github.com>
2023-12-25 19:04:05 +0530

    Create RR.c
    round robin algorithm

---
+++ RR.c
@@ -0,0 +1,65 @@
+#include<stdio.h>
+void main()
+{
+    //  fcfs with pre emption 
+    int n,i,qt,count=0,temp=0,sq=0,bt[10],wt[10],tat[10],rem_bt[10];
+    float awt=0,atat=0;
+    printf("Enter no. of Process: ");
+    scanf("%d",&n);
+    printf("Enter Burst Time of Process: ");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&bt[i]);
+        rem_bt[i]=bt[i];
+    }
+    printf("Enter the Quantum Time: ");
+    scanf("%d",&qt);
+    while(1)
+    {
+        for(i=0,count=0;i<n;i++)
+        {
+            temp=qt;
+            if(rem_bt[i]==0)
+            {
+                count++;
+                continue;
+            }
+            if(rem_bt[i]>qt)
+            {
+                rem_bt[i]=rem_bt[i]-qt;
+            }
+            else
+                if(rem_bt>=0)
+                {
+                    temp=rem_bt[i];
+                    rem_bt[i]=0;
+                }
+                sq=sq+temp;
+                tat[i]=sq;
+        }
+        if(n==count)
+            break;
+    }
+    printf("\nProcess\tBurst Time\tTurn Around Time\tWaiting Time\n");
+        for(i=0;i<n;i++)
+        {
+            wt[i]=tat[i]-bt[i];
+            awt=awt+wt[i];
+            atat=atat+tat[i];
+            printf("\n%d\t%d\t\t%d\t\t\t%d",i+1,bt[i],tat[i],wt[i]);
+        }
+        printf("\nAverage Waiting Time = %f\n",awt/n);
+        printf("Average Turn Around Time = %f\n",atat/n);
+
+}
+
+
+OUTPUT:
+Process Burst Time      Turn Around Time        Waiting Time
+
+1       5               13                      8
+2       3               10                      7
+3       1               5                       4
+4       4               12                      8
+Average Waiting Time = 6.750000
+Average Turn Around Time = 10.000000
